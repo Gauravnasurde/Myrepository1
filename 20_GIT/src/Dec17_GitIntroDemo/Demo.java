db388ae635f8bc1ab4b1086081981eb94fd6cd9d
Gauravnasurde <96283099+Gauravnasurde@users.noreply.github.com>
2021-12-20 21:51:02 +0530

    Update Demo.java

--- 20_GIT/src/Dec17_GitIntroDemo/Demo.java
+++ 20_GIT/src/Dec17_GitIntroDemo/Demo.java
@@ -2,12 +2,18 @@ package Dec17_GitIntroDemo;
 
 public class Demo {
 
+	public void m1()
+	{
+		System.out.println("Branch code");
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("Hi hello");
 		System.out.println("git hub");
 		System.out.println("After Adding token");
                 System.out.println("Pull request")
+			Demo d=new Demo();
+		             d.m1();
 	}
 
 }
