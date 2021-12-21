b66be7a53424818c74e4f5e40ac5cfb0a08fb7e9
Shree <Shree@DESKTOP-DSJJ1OM>
2021-12-21 17:03:55 +0530

    new Class in 1st repo

---
+++ 20_GIT/src/Dec17_GitIntroDemo/Demo2.java
@@ -0,0 +1,14 @@
+package Dec17_GitIntroDemo;
+
+public class Demo2 
+{
+	
+	int x=20;
+	
+	String y="abx";
+	public static void main(String[] args) 
+	{
+		System.out.println("Another Method");
+	}
+
+}
